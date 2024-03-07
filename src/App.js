5775202cdc3e7f83d1a7d224e409b983d87f7693
Danish Asghar <danish.2909@icloud.com>
2024-03-07 19:58:01 +0530

    implement quiz start button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Quiz from './component/quiz';
-
+import { useState } from 'react';
 function App() {
+  const [ showQuiz , setShowQuiz ] = useState(false)
+  const handleStartQuiz =()=>{
+    setShowQuiz(!showQuiz)
+  }
   return (
     <div className="App">
-      <Quiz/>
+      {showQuiz ? <Quiz/> : <button onClick={handleStartQuiz}>Start Quiz</button>}
+      
     </div>
   );
 }
